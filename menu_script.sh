047f181102c33a9820e56dbbe2b7f44393cb1b28
Nour Abdelaziz <nour.abdelaziz.mohamed@gmail.com>
2023-12-04 18:39:33 +0200

    exam update

--- menu_script.sh
+++ menu_script.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
-while true; 
+echo "Welcome Program"
+showdate()
+{
+
+	date  
+}
+
+showSysUptime()
+{
+
+	uptime
+}
+showCurrentUsers()
+{
+	whoami
+}
+ input = 1
+while [ $choice != 0 ]
 do
-	echo "welcome"
-	echo " 1 .Display date & time :"
-	echo " 2 .uptime :"
-	echo " 3 .info about system users login :"
-	echo " 4.press '8' quit :"
-	
-	read -p input
-	case $input in
-1)
-		now="$(date)"
-
-		printf"Current date and time %s\n" "$now"
-
-		now="$(date +'%d/%m/%Y')"
-		printf "Current date in dd/mm/yyyy formate %s\n" "$now"
-
-		echo "Starting backup at $now ,please wait..."
-	;;
-	2)
-		uptime
-		;;
-	3)
-	   whoami
-	;;
-	8)
-	
-	echo "Exit"
-	exit 0
-	;;
-	esac
-	done
-
+    
+    echo "1.Display the current date and time"
+    echo "2. Display the system uptime"
+    echo "3. Display system loggedin users"
+    echo "Enter 0 to Quit"
+    echo "Input:"
+    read -r choice
+    case $choice in
+        1)
+            showdate 
+            ;;
+        2)
+            showSysUptime 
+            ;;
+        3)
+            
+           showCurrentUsers 
+            ;;
+        0)
+            echo "Exit Good bye!"
+            ;;
+        *)
+            echo "ERROR RETRY !"
+            ;;
+    esac
+done
